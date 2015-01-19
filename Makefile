0ca0bb8597c85dc222957ee951989ee1b9a5a071
Oscar Ace?a <oscaracena@gmail.com>
2015-01-19 18:50:29 +0100

    Debian/pip package files

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 # -*- mode: makefile-gmake; coding: utf-8 -*-
 
 all:
-	make -C mibanda $@
 
 .PHONY: clean
 clean:
 	find -name "*.pyc" -delete
 	find -name "*~" -delete
-	find -name "*.so" -delete
-	find -name "*.o" -delete
+	$(RM) -fr dist MANIFEST
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python -u
+# -*- mode: python; coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name = 'mibanda',
+    version = "0.20150119",
+    description = "Library to control the Xiaomi Mi Band",
+    author = "Oscar Acena",
+    author_email = "oscar.acena@gmail.com",
+    url = "https://bitbucket.org/OscarAcena/mibanda",
+
+    packages = ['mibanda'],
+)
